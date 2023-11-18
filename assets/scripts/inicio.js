31a6c940ca4399857b2a1d081fef6e9b258838ca
Pedro Moreira <pedrogonmoreira@gmail.com>
2023-11-17 21:48:54 -0300

    ajuste inicio html css e js

--- assets/scripts/inicio.js
+++ assets/scripts/inicio.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loadingHub = document.getElementById("container-home");
-    const mainCarousel = document.getElementById("main-carousel");
+    const skipBtn = document.getElementById("skip-btn");
+    const mainCarousel = document.getElementById("main-carrousel");
 
-    // Certifique-se de que os elementos existam antes de tentar alterar seus estilos
-    if (loadingHub && mainCarousel) {
-      loadingHub.style.display = "flex";
-      mainCarousel.style.display = "none";
+    if (loadingHub && mainCarousel && skipBtn) {
+        loadingHub.style.display = "flex";
+        skipBtn.style.display = "none";
+        mainCarousel.style.display = "none";
 
-      function toggleElements() {
-        loadingHub.style.display = "none";
-        mainCarousel.style.display = "block";
-      }
+        function toggleElements() {
+            loadingHub.style.display = "none";
+            skipBtn.style.display = "block";
+            mainCarousel.style.display = "block";
+        }
 
-      setTimeout(toggleElements, 2500);
+        setTimeout(toggleElements, 2500);
     }
-  });
+});
